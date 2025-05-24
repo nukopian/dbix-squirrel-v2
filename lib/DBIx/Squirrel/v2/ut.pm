d163d15f52b02ef3b51b41e9ef90dadfdf678a9f
nukopian <nukopian@users.noreply.github.com>
2025-05-24 16:40:08 +0100

    feat: added code
    More imports from Ref::Util and Carp

--- lib/DBIx/Squirrel/v2/ut.pm
+++ lib/DBIx/Squirrel/v2/ut.pm
@@ -3,18 +3,34 @@ package    # hide from PAUSE
 use v5.38;
 use parent 'Exporter';
 
+use Carp qw(
+    carp
+    confess
+    croak
+);
 use Ref::Util qw(
     is_arrayref
     is_blessed_ref
     is_coderef
     is_hashref
+    is_scalarref
+    is_plain_arrayref
+    is_plain_coderef
+    is_plain_hashref
 );
 
 our @EXPORT_OK = qw(
+    carp
+    confess
+    croak
     is_arrayref
     is_blessed_ref
     is_coderef
     is_hashref
+    is_scalarref
+    is_plain_arrayref
+    is_plain_coderef
+    is_plain_hashref
 );
 
 1;
